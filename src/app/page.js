2fc52cd3828b42fa5ddd07d9f4bb354ac95fd212
manish chauhan <manishkirthalya@gmail.com>
2024-03-10 02:49:44 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Navigation from "@/component/Navigation";
 
 export default function Home() {
   return (
     <>
+    <Navigation/>
     <h1>start</h1>
     </>
   );
