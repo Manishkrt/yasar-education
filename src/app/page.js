d1bd5e1a8efa7f7e3a3472ef227636d098c8c5cf
vikash98711 <vikashrstylo123456@gmail.com>
2024-03-10 03:28:33 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navigation from "@/component/Navigation";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
     <Navigation/>
-    <h1 className="container bg-primary" >start</h1>
+    <h1 className="container bg-dark" >start</h1>
+    <Link href="#"></Link>
     </>
   );
 }
