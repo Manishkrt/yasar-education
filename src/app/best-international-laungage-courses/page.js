77f3bc0d9748c51bfd7a7eb07af9013ba6be8a6b
manishkrt <manishkirthalya@gmail.com>
2024-04-01 22:26:21 +0530

    update

--- src/app/best-international-laungage-courses/page.js
+++ src/app/best-international-laungage-courses/page.js
@@ -13,72 +13,11 @@ const page = () => {
     <>
       <Navigation />
 
-<<<<<<< HEAD
-      <CoomonStudentSlider />
-      <section className="course-two course-two--page">
-        <div
-          className="course-two__shape-top wow fadeInRight animated"
 
-        >
-          <img src="assets/images/shapes/course-shape-1.png" alt="eduact" />
-=======
-   <CoomonStudentSlider/>
+
+   <CoomonStudentSlider/> 
    <section className="course-two course-two--page pt-pb">
-  <div
-    className="course-two__shape-top wow fadeInRight animated"
-   
-  >
-    <img src="assets/images/shapes/course-shape-1.png" alt="eduact" />
-  </div>
-  <div className="container">
-    <div className="row">
-      <div
-        className="col-xl-4 col-md-6 wow fadeInUp animated"
-       
-      >
-        <div className="course-two__item">
-          <div className="course-two__thumb">
-            <img src="/assets/img/course-2-1.png" alt="eduact" />
-            <a className="course-two__like" href="javascript:void(0);">
-              <span className="icon-like fa-regular fa-heart" />
-            </a>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 353 177">
-              <path d="M37 0C16.5655 0 0 16.5655 0 37V93.4816C0 103.547 4.00259 113.295 11.7361 119.737C54.2735 155.171 112.403 177 176.496 177C240.589 177 298.718 155.171 341.261 119.737C348.996 113.295 353 103.546 353 93.4795V37C353 16.5655 336.435 0 316 0H37Z" />
-            </svg>
-          </div>
-          {/* /.course-thumb */}
-          <div className="course-two__content">
-            <div className="course-two__time">20 Hours</div>
-            <div className="course-two__ratings">
-              <span className="icon-star fa-solid fa-star" />
-              <span className="icon-star fa-solid fa-star" />
-              <span className="icon-star fa-solid fa-star" />
-              <span className="icon-star fa-solid fa-star" />
-              <span className="icon-star fa-solid fa-star" />
-              <div className="course-two__ratings__reviews">(25 Reviews)</div>
-            </div>
-            <h3 className="course-two__title">
-              <a href="management-consulting.html">
-                Management Consultants in Competitive Markets
-              </a>
-            </h3>
-            <div className="course-two__bottom">
-              <div className="course-two__author">
-                <img src="assets/images/course/author-1.png" alt="eduact" />
-                <h5 className="course-two__author__name">Guy Hawkins</h5>
-                <p className="course-two__author__designation">
-                  Project Manager
-                </p>
-              </div>
-              <div className="course-two__meta">
-                <h4 className="course-two__meta__price">$473.00</h4>
-                <p className="course-two__meta__class">15 Lessons</p>
-              </div>
-            </div>
-          </div>
-          {/* /.course-content */}
->>>>>>> 5393f024eca972ec0f7df3b6df78a46629034bc0
-        </div>
+  
         <div className="container">
           <div className="row">
             {CourseList.map((courseValue)=>( 
@@ -414,12 +353,7 @@ const page = () => {
             </div> */}
           </div>
         </div>
-        <div
-          className="course-two__shape-bottom wow fadeInLeft animated"
-
-        >
-          <img src="assets/images/shapes/course-shape-2.png" alt="eduact" />
-        </div>
+        
       </section>
 
 
