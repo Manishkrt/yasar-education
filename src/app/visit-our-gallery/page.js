c8596ec801c6372734c80e26c57f64bac9113755
vikash98711 <vikashrstylo123456@gmail.com>
2024-04-13 16:45:18 +0530

    update

--- src/app/visit-our-gallery/page.js
+++ src/app/visit-our-gallery/page.js
@@ -2,15 +2,37 @@
 import CompanyLogoSlider from '@/component/CompanyLogoSlider';
 import CoomonStudentSlider from '@/component/CoomonStudentSlider';
 import Footer from '@/component/Footer';
-import Navigation from '@/component/Navigation';
+// import Navigation from '@/component/Navigation';
 import QuiryForm from '@/component/QuiryForm';
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Tab, Tabs } from 'react-bootstrap';
+import { Tab, Tabs,Modal  } from 'react-bootstrap';
+
+
 const page = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalImages, setModalImages] = useState([]);
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+
+  const openModal = (images, index) => {
+    setModalImages(images);
+    setActiveImageIndex(index);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const images = [
+    'assets/img/gallery-1.jpg',
+    'assets/img/gallery-2.jpg',
+    'assets/img/gallery-3.jpg',
+    // Add more image URLs as needed
+  ];
   return (
    <>
-     <Navigation/>
+     {/* <Navigation/> */}
    <CoomonStudentSlider/>
    <>
    
@@ -19,23 +41,12 @@ const page = () => {
     <h2>GALLERY & VIDEOS
   
   
-  {/* <svg
-    className="arrow-svg"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 55 13"
-  >
-    <g clipPath="url(#clip0_324_36194)">
-      <path d="M10.5406 6.49995L0.700562 12.1799V8.56995L4.29056 6.49995L0.700562 4.42995V0.819946L10.5406 6.49995Z" />
-      <path d="M25.1706 6.49995L15.3306 12.1799V8.56995L18.9206 6.49995L15.3306 4.42995V0.819946L25.1706 6.49995Z" />
-      <path d="M39.7906 6.49995L29.9506 12.1799V8.56995L33.5406 6.49995L29.9506 4.42995V0.819946L39.7906 6.49995Z" />
-      <path d="M54.4206 6.49995L44.5806 12.1799V8.56995L48.1706 6.49995L44.5806 4.42995V0.819946L54.4206 6.49995Z" />
-    </g>
-  </svg> */}
+  
   </h2>
 </div>
     <div className='Gallery_tabs-wrapper'>
   <Tabs defaultActiveKey="Gallery" id="Gallery" className='p-3'>
-                  <Tab eventKey="Gallery" title="Gallery">
+                  {/* <Tab eventKey="Gallery" title="Gallery">
                     <div className='tabs-content mt-4'>
                       <div className="course-details__curriculum__title">
                       <section className="gallery-page">
@@ -58,7 +69,7 @@ const page = () => {
                         </h4>
                         </div>
       <div className="row">
-        {/* gallery-item-start */}
+    
         <div className="col-lg-4 col-md-6">
           <div className="gallery-page__single">
             <img src="assets/img/gallery-1.jpg" alt="eduact" />
@@ -70,8 +81,7 @@ const page = () => {
             </div>
           </div>
         </div>
-        {/* gallery-item-end */}
-        {/* gallery-item-start */}
+  
         <div className="col-lg-4 col-md-6">
           <div className="gallery-page__single">
             <img src="assets/img/gallery-2.jpg" alt="eduact" />
@@ -83,8 +93,7 @@ const page = () => {
             </div>
           </div>
         </div>
-        {/* gallery-item-end */}
-        {/* gallery-item-start */}
+    
         <div className="col-lg-4 col-md-6">
           <div className="gallery-page__single">
             <img src="assets/img/gallery-3.jpg" alt="eduact" />
@@ -96,8 +105,7 @@ const page = () => {
             </div>
           </div>
         </div>
-        {/* gallery-item-end */}
-        {/* gallery-item-start */}
+       
         <div className="col-lg-4 col-md-6">
           <div className="gallery-page__single">
             <img src="assets/img/gallery-2.jpg" alt="eduact" />
@@ -109,8 +117,7 @@ const page = () => {
             </div>
           </div>
         </div>
-        {/* gallery-item-end */}
-        {/* gallery-item-start */}
+  
         <div className="col-lg-4 col-md-6">
           <div className="gallery-page__single">
             <img src="assets/img/gallery-5.jpg" alt="eduact" />
@@ -122,8 +129,7 @@ const page = () => {
             </div>
           </div>
         </div>
-        {/* gallery-item-end */}
-        {/* gallery-item-start */}
+   
         <div className="col-lg-4 col-md-6">
           <div className="gallery-page__single">
             <img src="assets/img/gallery-1.jpg" alt="eduact" />
@@ -135,7 +141,7 @@ const page = () => {
             </div>
           </div>
         </div>
-        {/* gallery-item-end */}
+   
       </div>
     </div>
   </section>
@@ -143,7 +149,27 @@ const page = () => {
                       </div>
 
                     </div>
-                  </Tab>
+                  </Tab> */}
+                   <Tab eventKey="Gallery" title="Gallery">
+          <div className="tabs-content mt-4">
+            <div className="row">
+              {images.map((image, index) => (
+                <div className="col-lg-4 col-md-6" key={index}>
+                  <div
+                    className="gallery-page__single"
+                    onClick={() => openModal(images, index)}
+                  >
+                    <img src={image} alt={`Image ${index}`} />
+                    <div className="gallery-page__icon">
+                      <a className="img-popup" />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </Tab>
+      
                   <Tab eventKey="Video" title="Video">
                     <div className="tabs-content">
                       <div className="course-details__curriculum">
@@ -216,6 +242,17 @@ const page = () => {
                   
                   
                 </Tabs>
+                <Modal show={showModal} onHide={closeModal} centered>
+        <Modal.Body>
+          <div className='text-center'>
+            <img
+              src={modalImages[activeImageIndex]}
+              alt={`Image ${activeImageIndex}`}
+              style={{ maxWidth: '100%', maxHeight: '80vh', objectFit: 'cover' }}
+            />
+          </div>
+        </Modal.Body>
+      </Modal>
                 </div>
    
   </section>
