ab0783f948391c6d3b15cd30e5e42dedc4b0354c
vikash98711 <vikashrstylo123456@gmail.com>
2024-04-13 17:33:33 +0530

    update

--- src/app/visit-our-gallery/page.js
+++ src/app/visit-our-gallery/page.js
@@ -14,6 +14,7 @@ import './page.css'
 
 const page = () => {
   const [showModal, setShowModal] = useState(false);
+  
   const [modalImages, setModalImages] = useState([]);
   const [activeImageIndex, setActiveImageIndex] = useState(0);
 
